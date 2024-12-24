458819953e3a9bb0400691ee3de2e33b6a0a5a94
jonathanhunt <jhun728@wgu.edu>
2024-12-23 18:42:06 -0600

    Implemented and corrected logic for count_chars function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    #print(count_words(text))
+    print(count_chars(text))
     print_report(book_path, count_chars(text))
 
 
@@ -17,12 +17,13 @@ def count_chars(text):
     lowered_text = text.lower()
     letter_count_map = {}
     for letter in lowered_text:
+        letter = letter.lower()
         if letter not in letter_count_map and letter.isalpha():
             letter_count_map[letter] = 0
         elif letter.isalpha():
             letter_count_map[letter] += 1
-        return letter_count_map
-    
+    return letter_count_map
+
 def print_report(book_path, chars_map):
     #chars_map.sorted(reverse=True)
     print(f"--- Begin report of {book_path} ---\n")
